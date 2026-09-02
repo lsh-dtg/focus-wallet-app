6eb625d41bb697521d8d12007c2fa18eb09b26a7
lsh-dtg <lsh.kosa@gmail.com>
2026-09-02 22:34:33 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath,
-  images: { unoptimized: true },
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
