fa4126018dd95759a6362780faab629370eb1b81
lsh-dtg <lsh.kosa@gmail.com>
2026-09-02 22:36:18 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: basePath,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     unoptimized: true,
   },
